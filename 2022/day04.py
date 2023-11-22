542547765df5139c3152d7838b11f72fbc9da3ba
Stefan <mohrst84031@th-nuernberg.de>
2023-11-22 23:38:38 +0100

    Solve 2022 day 4 1 + 2

--- 2022/day04.py
+++ 2022/day04.py
@@ -1,6 +1,6 @@
 # TIM 2023
 # Advent of Code
-# Tag 1: Aufgabenname
+# Tag 4: Camp Cleanup
 # Autor: Stefan Mohr (stefan.mo)
 # Requirements: keine
 
@@ -11,27 +11,37 @@ def read_puzzle(filename):
     with open("inputs/" + filename, "r") as f:
         return f.read().splitlines()
 
-def compare_shifts(shift):
-    shift_1 = shift[:shift.find(",")]
-    shift_2 = shift[shift.find(",") + 1:]
-    shift_1 = {"start": shift_1[:shift_1.find("-")], "end": shift_1[shift_1.find("-") + 1:]}
-    shift_2 = {"start": shift_2[:shift_2.find("-")], "end": shift_2[shift_2.find("-") + 1:]}
-    if shift_1["start"] >= shift_2["start"] and shift_1["end"] <= shift_2["end"] or shift_2["start"] >= shift_1["start"] and shift_2["end"] <= shift_1["end"]:
-        print(shift_1, shift_2)
+def find_fully_containing_ranges(shift):
+    shift_1 = reformat_shifts(shift)[0]
+    shift_2 = reformat_shifts(shift)[1]
+    if shift_1["start"] >= shift_2["start"] and shift_1["end"] <= shift_2["end"]:
         return True
-    # elif shift_2["start"] >= shift_1["start"] and shift_2["end"] <= shift_1["end"]:
-    #     print(shift_1, shift_2)
-    #     return True
+    elif shift_2["start"] >= shift_1["start"] and shift_2["end"] <= shift_1["end"]:
+        return True
     else:
         return False
 
+
+def find_partially_containing_ranges(shift):
+    shift_1 = reformat_shifts(shift)[0]
+    shift_2 = reformat_shifts(shift)[1]
+    return False if shift_1["end"] < shift_2["start"] or shift_2["end"] < shift_1["start"] else True
+
+def reformat_shifts(shift):
+    shift_1 = shift[:shift.find(",")]
+    shift_2 = shift[shift.find(",") + 1:]
+    shift_1 = {"start": int(shift_1[:shift_1.find("-")]), "end": int(shift_1[shift_1.find("-") + 1:])}
+    shift_2 = {"start": int(shift_2[:shift_2.find("-")]), "end": int(shift_2[shift_2.find("-") + 1:])}
+    return shift_1, shift_2
+
 def solve_part_1(puzzle):
-    assignment_pairs = len(list(filter(compare_shifts, puzzle)))
+    assignment_pairs = len(list(filter(find_fully_containing_ranges, puzzle)))
     return assignment_pairs
 
 
 def solve_part_2(puzzle):
-    return 2
+    assignment_pairs = len(list(filter(find_partially_containing_ranges, puzzle)))
+    return assignment_pairs
 
 
 if __name__ == "__main__":
--- 2022/day04_test.py
+++ 2022/day04_test.py
@@ -1,21 +1,21 @@
 # TIM 2022
 # Advent of Code
-# Tag 1: Aufgabenname
+# Tag 4: Camp Cleanup
 # Autor: Stefan Mohr (stefan.mo)
 # Requirements: pytest
 
 from day04 import solve_part_1, solve_part_2, read_puzzle
 
 
-def dummy_puzzle():
-    return read_puzzle("day04_example1.txt")
+def dummy_puzzle(filename):
+    return read_puzzle(filename)
 
 
 def test_part_1():
-    puzzle = dummy_puzzle()
+    puzzle = dummy_puzzle("day04_example1.txt")
     assert solve_part_1(puzzle) == 2
 
 
 def test_part_2():
-    puzzle = dummy_puzzle()
-    assert solve_part_2(puzzle) == 2
+    puzzle = dummy_puzzle("day04_example1.txt")
+    assert solve_part_2(puzzle) == 4
