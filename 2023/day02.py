cefc0a1ff540abb74c08d6d1f8c52fb6a792e328
Stefan <mohrst84031@th-nuernberg.de>
2023-12-02 22:11:22 +0100

    Refactor 2023 day 02 part 1

--- 2023/day02.py
+++ 2023/day02.py
@@ -14,7 +14,7 @@ def read_puzzle(filename):
 
 
 def get_power_of_set(game):
-    draws = list(map(lambda x: reversed(tuple(x.split(" "))), map(str.strip, game.replace(";", ",").split(","))))
+    draws = list(map(lambda x: tuple(reversed(x.split())), map(str.strip, game.replace(";", ",").split(","))))
     min_load = {"red": 0, "green": 0, "blue": 0}
     for cube in draws:
         color, value = cube
@@ -23,19 +23,15 @@ def get_power_of_set(game):
     return prod(min_load.values())
 
 
-def control_set(game_set):
-    cubes = {"red": 12, "green": 13, "blue": 14}
-    set_tuples = list(map(lambda x: tuple(x.split(" ")), map(str.strip, game_set.split(","))))
-    possible = list(map(lambda x: int(x[0]) <= cubes[x[1]], set_tuples))
-    return False not in possible
-
-
 def get_sets(game):
-    return list(map(control_set, map(str.strip, game.split(";"))))
+    cubes = {"red": 12, "green": 13, "blue": 14}
+    draws = list(map(lambda x: tuple(reversed(x.split())), map(str.strip, game.replace(";", ",").split(","))))
+    x = list(map(lambda x: int(x[1]) <= cubes[x[0]], draws))
+    return False not in x
 
 
 def solve_part_1(puzzle):
-    return sum(list(map(lambda x: x[0] + 1 if False not in get_sets(x[1]) else 0, enumerate(puzzle))))
+    return sum(list(map(lambda x: x[0] + 1 if get_sets(x[1]) else 0, enumerate(puzzle))))
 
 
 def solve_part_2(puzzle):
